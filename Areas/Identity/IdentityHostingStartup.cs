6a968a695c8361de82971c9d11314f95b75145be
Onea Radu <onearadu12@gmail.com>
2021-05-12 19:31:17 +0300

    Identity scaffolding down

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EmagClone.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OldIronIronWeTake.Data;
+
+[assembly: HostingStartup(typeof(EmagClone.Areas.Identity.IdentityHostingStartup))]
+namespace EmagClone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
