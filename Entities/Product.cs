a9bad70d61f30e4b85be57942bf6fa275c9d826d
vladirares <jeleavladimir@yahoo.ro>
2021-05-14 14:54:47 +0300

    ProducService

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -6,6 +6,7 @@ namespace EmagClone.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Stock { get; set; }
         // Todo stock/Reviews
         public Guid SellerId { get; set; }
         public virtual User Seller { get; set; }
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EmagClone.Entities;
+using OldIronIronWeTake.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,79 @@ namespace EmagClone.Services
 {
     public class ProductService
     {
+
+        private readonly ApplicationDbContext context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Product> GetAll()
+        {
+            return context.Products.ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return context.Products.Find(id);
+        }
+
+        public bool Post( String name, Guid userId )
+        {
+
+            User user = context.Users.Find(userId);
+            
+            if (user == null)
+            {
+                return false;
+            }
+
+            context.Products.Add(new Product { Name = name, Seller = user });
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Update( String nume, int id)
+        {
+            Product product = context.Products.Find(id);
+
+            if ( product == null )
+            {
+                return false;
+            }
+
+            product.Name = nume;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Update(Guid sellerId, int id)
+        {
+            Product product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return false;
+            }
+
+            product.SellerId = sellerId;
+            context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            Product product = context.Products.Find(id);
+            if (product == null)
+            {
+                return false;
+            }
+            context.Remove(product);
+            context.SaveChanges();
+            return true;
+
+        }
+
     }
 }
