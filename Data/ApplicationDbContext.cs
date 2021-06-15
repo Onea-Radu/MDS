466a05e24432bc6a2e04f9c6a63e4b7fbe442262
dariahazaparu <61266022+dariahazaparu@users.noreply.github.com>
2021-06-15 15:32:09 +0300

    fix: dbset for order

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -21,7 +21,7 @@ namespace OldIronIronWeTake.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Problem> Problems { get; set; }
-
+        public DbSet<Order> Orders { get; set; }
 
     }
 }
