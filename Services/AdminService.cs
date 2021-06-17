b8a40e0d31a27307b58f8869cc762b147960f2e3
vladirares <jeleavladimir@yahoo.ro>
2021-06-17 21:25:56 +0300

    adminService

---
+++ Services/AdminService.cs
@@ -0,0 +1,30 @@
+﻿using EmagClone.Entities;
+using Microsoft.AspNetCore.Identity;
+using OldIronIronWeTake.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmagClone.Services
+{
+    public class AdminService
+    {
+
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<User> userManager;
+
+        public AdminService(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        public bool deleteUser(int id)
+        {
+            //to DO
+            return true;
+        }
+
+    }
+}
